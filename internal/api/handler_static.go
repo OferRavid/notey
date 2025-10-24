7d9bded83699b4c303222e730afee9508f401c23
OferRavid <ofer.rvd@gmail.com>
2025-10-24 19:34:48 +0300

    Added forms to front-end

--- internal/api/handler_static.go
+++ internal/api/handler_static.go
@@ -12,7 +12,36 @@ func (cfg *ApiConfig) ServeStaticFiles(c echo.Context) error {
 	cfg.PageVisitsGauge.Inc()
 
 	// Strip the prefix /app and serve the requested file
-	filePath := c.Request().URL.Path[len("/app/"):]
+	requestPath := c.Request().URL.Path[len("/app/"):]
+	filePath := ""
+
+	switch requestPath {
+	case "":
+		break
+	case "contact":
+		filePath = "contact/contact.html"
+	case "login":
+		filePath = "login/login.html"
+	case "400":
+		filePath = "bad-request/bad-request.html"
+	case "401":
+		filePath = "authorization-error/unauthorized.html"
+	case "403":
+		filePath = "access-denied/access-denied.html"
+	case "404":
+		filePath = "not-found/not-found.html"
+	case "500":
+		filePath = "server-error/server-error.html"
+	case "privacy-policy":
+		filePath = "policy/privacy.html"
+	case "terms":
+		filePath = "policy/terms.html"
+	case "notes":
+		filePath = "notes/notes.html"
+	case "note":
+		filePath = "notes/note.html"
+	}
 	staticPath := filepath.Join(cfg.StaticDir, filePath)
+
 	return c.File(staticPath)
 }
